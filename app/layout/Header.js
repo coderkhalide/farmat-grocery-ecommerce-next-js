1b5e2a4594affe41d4c3922fb261b5e758d865df
Khalid Saifullah <khalideofficial@gmail.com>
2022-09-02 00:53:25 +0600

    Project Setup

---
+++ app/layout/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="bg-white">
+      <div className="container">
+        Hello
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import Layout from '../app/layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
