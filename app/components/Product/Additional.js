e71ae05fd76a4d92266e7b925f0263dcc697d71b
Khalid Saifullah <khalideofficial@gmail.com>
2022-09-16 01:52:25 +0600

    product info

--- app/components/Product/Additional.js
+++ app/components/Product/Additional.js
@@ -1,8 +1,17 @@
-import React from 'react'
 
 const Additional = () => {
   return (
-    <div>Additional</div>
+    <div>
+      <h1 className="text-title md:text-xl text-lg font-bold mb-5">Additional information</h1>
+      <div className="flex  border">
+        <div className="w-1/2 py-2 px-4 text-sm bg-gray-100">
+          Weight
+        </div>
+        <div className="w-1/2 py-2 px-4 text-sm">
+          500g
+        </div>
+      </div>
+    </div>
   )
 }
 
--- app/components/Product/Description.js
+++ app/components/Product/Description.js
@@ -1,8 +1,137 @@
-import React from 'react'
+import { useState } from "react";
+import draftToHtml from "draftjs-to-html";
+
+const BODY = {
+  "blocks": [
+    {
+      "key": "p0t6",
+      "text": "Hello",
+      "type": "header-one",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {
+        "text-align": "start"
+      }
+    },
+    {
+      "key": "6rrel",
+      "text": "Raised without antibiotics and full of flavor, this beef is the base of big, juicy burgers, savory meat loaf and rich Bolognese sauce. You can enjoy this delicious local ground beef for your meatloaf, burgers, meatballs, shepherd’s pie, spicy taco meat and so much more.",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {
+          "offset": 0,
+          "length": 270,
+          "style": "color-rgb(102,102,102)"
+        },
+        {
+          "offset": 0,
+          "length": 270,
+          "style": "bgcolor-rgb(255,255,255)"
+        },
+        {
+          "offset": 0,
+          "length": 270,
+          "style": "fontsize-14"
+        },
+        {
+          "offset": 0,
+          "length": 270,
+          "style": "fontfamily-muli, sans-serif"
+        }
+      ],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "f4rt5",
+      "text": "As one of Farmart’s premium beef suppliers, Local Angus works with a coalition of small family farms throughout the Mid-Atlantic region* who feed their cattle a diet of primarily grass, supplemented by grain throughout the finishing months. Every farm in this program is independently audited for animal welfare practices to ensure the best standards of care. ",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {
+          "offset": 0,
+          "length": 43,
+          "style": "color-rgb(102,102,102)"
+        },
+        {
+          "offset": 44,
+          "length": 11,
+          "style": "color-rgb(102,102,102)"
+        },
+        {
+          "offset": 56,
+          "length": 303,
+          "style": "color-rgb(102,102,102)"
+        },
+        {
+          "offset": 0,
+          "length": 43,
+          "style": "bgcolor-rgb(255,255,255)"
+        },
+        {
+          "offset": 44,
+          "length": 11,
+          "style": "bgcolor-rgb(255,255,255)"
+        },
+        {
+          "offset": 56,
+          "length": 303,
+          "style": "bgcolor-rgb(255,255,255)"
+        },
+        {
+          "offset": 0,
+          "length": 43,
+          "style": "fontsize-14"
+        },
+        {
+          "offset": 44,
+          "length": 11,
+          "style": "fontsize-14"
+        },
+        {
+          "offset": 56,
+          "length": 303,
+          "style": "fontsize-14"
+        },
+        {
+          "offset": 0,
+          "length": 43,
+          "style": "fontfamily-muli, sans-serif"
+        },
+        {
+          "offset": 44,
+          "length": 11,
+          "style": "fontfamily-muli, sans-serif"
+        },
+        {
+          "offset": 56,
+          "length": 303,
+          "style": "fontfamily-muli, sans-serif"
+        },
+        {
+          "offset": 44,
+          "length": 11,
+          "style": "BOLD"
+        }
+      ],
+      "entityRanges": [],
+      "data": {
+        "text-align": "start"
+      }
+    }
+  ],
+  "entityMap": {}
+}
 
 const Description = () => {
+  const [local, setLocal] = useState(draftToHtml(BODY));
+
   return (
-    <div>Description</div>
+    <div className="prose prose-sm">
+      <div className="preview" dangerouslySetInnerHTML={{ __html: local }} />
+    </div>
   )
 }
 
